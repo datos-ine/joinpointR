7ab2b002e7b0562b55f555f64e0576f39dbee0dc
grupolepto <grupolepto@gmail.com>
2026-09-11 13:31:01 -0300

    Update 11-09-2026: testing model_jp_grid

--- R/model_jp.R
+++ R/model_jp.R
@@ -1,55 +1,20 @@
 #' Joinpoint Regression Models by Groups
 #'
-#' Fits segmented linear regression models by groups for age-standardized rates
-#' using joinpoint regression. Models can be fitted using either a stepwise
-#' selection procedure based on the Bayesian Information Criterion (BIC) or a
-#' fixed number of joinpoints. Internally, the function calls
-#' \code{segmented::selgmented()} or \code{segmented::segmented()} and applies
-#' a log transformation to the response variable.
+#' Fits segmented linear regression models for rates using joinpoint
+#' regression with an exhaustive Grid Search and the Bayesian Information
+#' Criterion (BIC) to select the optimal model with up to five joinpoints.
+#' Internally, the function applies a log transformation to the response
+#' variable.
 #'
-#' @param data A data frame containing age-standardized rates.
+#' @param data A data frame containing crude or age standardized rates.
 #' @param value Response variable.
 #' @param time Time variable.
-#' @param group Names of one or more grouping variables.
+#' @param group Names of the grouping variable(s). Defaults to NULL.
 #' @param k Maximum number of joinpoints to estimate.
 #' @param min_dist Minimum number of observations required per segment.
-#' Defaults to 3 to prevent joinpoints from being too close.
-#' @param step Logical. If \code{TRUE}, uses a stepwise procedure to select the
-#' number of joinpoints based on BIC. If \code{FALSE}, fits a model with a
-#' fixed number of joinpoints specified by \code{k}.
-#' @param test Logical. If \code{TRUE}, tests differences in slope t-values
-#' during the stepwise selection procedure. Only used when
-#' \code{step = TRUE}.
+#'
 #' @return A named list of joinpoint regression models by group.
 #' @author Tamara Ricardo
-#' @details
-#' The National Cancer Institute (NCI) recommends the following maximum number
-#' of joinpoints according to the length of the time series (Kim et al., 2000):
-#'
-#' \itemize{
-#' \item 0--6 time points: 0 joinpoints.
-#' \item 7--11 time points: 1 joinpoint.
-#' \item 12--16 time points: 2 joinpoints.
-#' \item 17--21 time points: 3 joinpoints.
-#' \item 22--26 time points: 4 joinpoints.
-#' \item 27--31 time points: 5 joinpoints.
-#' \item 32--36 time points: 6 joinpoints.
-#' \item 37 or more time points: 7 joinpoints.
-#' }
-#'
-#' @references
-#' Kim HJ, Fay MP, Feuer EJ, Midthune DN (2000).
-#' "Permutation Tests for Joinpoint Regression with Applications to Cancer Rates."
-#' \emph{Statistics in Medicine}, 19(3), 335--351.
-#' doi:10.1002/(sici)1097-0258(20000215)19:3<335::aid-sim336>3.0.co;2-z.
-#'
-#' Muggeo, V.M.R., Adelfio, G. (2011).
-#' Efficient change point detection in genomic sequences of continuous
-#' measurements. \emph{Bioinformatics}, 27, 161–166.
-#'
-#' Muggeo, Vito. (2020).
-#' Selecting number of breakpoints in segmented regression:
-#' implementation in the R package segmented. 10.13140/RG.2.2.12891.39201.
 #'
 #' @examples
 #' # Load example data
@@ -59,168 +24,519 @@
 #' levels(hiv_data$region)
 #'
 #' # Fit models
-#' mods <- model_jp(data = hiv_data, value = hiv_rate, time = year, group = c("region", "sex"),
-#'  k = 2, min_dist = 3, step = TRUE, test = TRUE)
+#' mods <- model_jp(
+#' data = hiv_data,
+#'  value = hiv_rate,
+#' time = year,
+#' group = c("region", "sex"),
+#' k = 2,
+#' min_dist = 3
+#' )
 #'
 #' # Show the output of the first model by calling its index
 #' mods[[1]]
 #'
 #' # Same output will be obtained when calling model name
-#' mods$Central
+#' mods$Central_Female
 #'
 #' @export
-
+#'
 model_jp <- function(
   data,
   value,
   time,
-  group,
+  group = NULL,
   k = 2,
-  min_dist = 3,
-  step = TRUE,
-  test = TRUE
+  min_dist = 2
 ) {
-  # ---- Define variables ----
+  # ---- Define response variable ----
   value <- rlang::ensym(value)
-  time <- rlang::ensym(time)
 
-  value_name <- rlang::as_string(value)
-  time_name <- rlang::as_string(time)
+  # ---- Define time variable ----
+  time <- rlang::ensym(time)
 
   # ---- Validate response variable ----
   if (any(data[[value]] <= 0, na.rm = TRUE)) {
-    stop("The response variable must be > 0 to apply log() transformation")
+    stop(
+      "The response variable must be > 0 to apply log() transformation"
+    )
   }
 
-  # ---- Validate grouping variable/s ----
+  # ---- Validate maximum number of joinpoints ----
+  if (k > 5) {
+    stop("This function currently supports a maximum of 5 joinpoints")
+  }
+
+  # ---- Validate optimal number of joinpoints ----
+  if (
+    k > 0 &
+      length(time) < 7 |
+      k > 1 & length(time) < 12 |
+      k > 2 & length(time) < 17 |
+      k > 3 & length(time) < 22 |
+      k > 4 & length(time) < 27
+  ) {
+    warning(
+      "The selected number of joinpoints is too high for your time series (check Details)"
+    )
+  }
+
+  # ---- Validate grouping variables ----
   if (!all(group %in% names(data))) {
     stop("Some grouping variable names are not present in data")
   }
 
-  # ---- Validate number of joinpoints ----
-  if (!step && k > 1 && dplyr::n_distinct(data[[time]]) < 11) {
-    message(
-      "Note: fitting two or more joinpoints is not recommended for short time series (see Details)."
-    )
-  }
-
-  # ---- Validate test and step arguments ----
-  if (!step && test) {
-    message("'test' is ignored when step = FALSE")
+  # ---- Validate number of grouping variables ----
+  if (length(group) > 2) {
+    stop("This function allows a maximum of two grouping variables")
   }
 
   # ---- Prepare data ----
-  data <- data |>
-    dplyr::mutate(
-      .jp_time = !!time,
-      .jp_log_value = log(!!value),
-      .jp_group = forcats::fct_cross(
-        !!!rlang::syms(group),
-        sep = "_",
-        keep_empty = FALSE
+  if (!is.null(group)) {
+    data <- data |>
+      dplyr::mutate(
+        .jp_time = !!time,
+        .jp_log_value = log(!!value),
+        .jp_group = forcats::fct_cross(
+          !!!rlang::syms(group),
+          sep = "_",
+          keep_empty = FALSE
+        )
+      ) |>
+      dplyr::arrange(.jp_group, .jp_time)
+  } else {
+    data <- data |>
+      dplyr::mutate(
+        .jp_time = !!time,
+        .jp_log_value = log(!!value),
+        .jp_group = NA_character_
+      ) |>
+      dplyr::arrange(.jp_time)
+  }
+
+  # ---- Define groups ----
+  groups <- dplyr::group_keys(
+    dplyr::group_by(data, .jp_group)
+  )$.jp_group
+
+  # ==========================================================
+  # Function for one group
+  # ==========================================================
+
+  fit_group <- function(data) {
+    # --------------------------------------------------------
+    # Fit segmented regression
+    # --------------------------------------------------------
+
+    fit_jp <- function(jp) {
+      x <- data$.jp_time
+
+      X <- data.frame(
+        y = data$.jp_log_value,
+        time = x
       )
-    ) |>
-    dplyr::group_by(.data$.jp_group)
 
-  # ---- Group names ----
-  groups <- dplyr::group_keys(data)$.jp_group
+      # ---- Create hinge functions ----
 
-  # ---- Fit the linear model ----
-  lm_fit <- function(.x) {
-    stats::lm(
-      .jp_log_value ~ .jp_time,
-      data = .x
+      if (length(jp) >= 1) {
+        X$U1.time <- pmax(x - jp[1], 0)
+      }
+
+      if (length(jp) >= 2) {
+        X$U2.time <- pmax(x - jp[2], 0)
+      }
+
+      if (length(jp) >= 3) {
+        X$U3.time <- pmax(x - jp[3], 0)
+      }
+
+      if (length(jp) >= 4) {
+        X$U4.time <- pmax(x - jp[4], 0)
+      }
+
+      if (length(jp) >= 5) {
+        X$U5.time <- pmax(x - jp[5], 0)
+      }
+
+      # ---- Fit model ----
+
+      if (length(jp) == 0) {
+        stats::lm(
+          y ~ time,
+          data = X
+        )
+      } else if (length(jp) == 1) {
+        stats::lm(
+          y ~ time + U1.time,
+          data = X
+        )
+      } else if (length(jp) == 2) {
+        stats::lm(
+          y ~ time + U1.time + U2.time,
+          data = X
+        )
+      } else if (length(jp) == 3) {
+        stats::lm(
+          y ~ time + U1.time + U2.time + U3.time,
+          data = X
+        )
+      } else if (length(jp) == 4) {
+        stats::lm(
+          y ~ time + U1.time + U2.time + U3.time + U4.time,
+          data = X
+        )
+      } else {
+        stats::lm(
+          y ~ time + U1.time + U2.time + U3.time + U4.time + U5.time,
+          data = X
+        )
+      }
+    }
+
+    # --------------------------------------------------------
+    # Validate joinpoints
+    # --------------------------------------------------------
+    valid_jp <- function(jp) {
+      idx <- match(
+        jp,
+        data$.jp_time
+      )
+
+      left <- idx[1] - 1
+
+      if (length(jp) > 1) {
+        middle <- diff(idx) - 1
+      } else {
+        middle <- numeric(0)
+      }
+
+      right <-
+        nrow(data) - idx[length(idx)]
+
+      all(
+        left >= min_dist,
+        middle >= min_dist,
+        right >= min_dist
+      )
+    }
+
+    # --------------------------------------------------------
+    # Model results
+    # --------------------------------------------------------
+    results <- list()
+
+    # --------------------------------------------------------
+    # 0 joinpoints
+    # --------------------------------------------------------
+    mod0 <- fit_jp(numeric(0))
+
+    results[[length(results) + 1]] <-
+      tibble::tibble(
+        n_jp = 0,
+        jp1 = NA_real_,
+        jp2 = NA_real_,
+        jp3 = NA_real_,
+        jp4 = NA_real_,
+        jp5 = NA_real_,
+        BIC = stats::BIC(mod0)
+      )
+
+    # --------------------------------------------------------
+    # 1 joinpoint
+    # --------------------------------------------------------
+    if (k >= 1) {
+      for (jp1 in data$.jp_time) {
+        jp <- c(jp1)
+
+        if (!valid_jp(jp)) {
+          next
+        }
+
+        mod <- fit_jp(jp)
+
+        results[[length(results) + 1]] <-
+          tibble::tibble(
+            n_jp = 1,
+            jp1 = jp1,
+            jp2 = NA_real_,
+            jp3 = NA_real_,
+            jp4 = NA_real_,
+            jp5 = NA_real_,
+            BIC = stats::BIC(mod)
+          )
+      }
+    }
+
+    # --------------------------------------------------------
+    # 2 joinpoints
+    # --------------------------------------------------------
+    if (k >= 2) {
+      for (jp1 in data$.jp_time) {
+        for (jp2 in data$.jp_time) {
+          if (jp2 <= jp1) {
+            next
+          }
+
+          jp <- c(jp1, jp2)
+
+          if (!valid_jp(jp)) {
+            next
+          }
+
+          mod <- fit_jp(jp)
+
+          results[[length(results) + 1]] <-
+            tibble::tibble(
+              n_jp = 2,
+              jp1 = jp1,
+              jp2 = jp2,
+              jp3 = NA_real_,
+              jp4 = NA_real_,
+              jp5 = NA_real_,
+              BIC = stats::BIC(mod)
+            )
+        }
+      }
+    }
+
+    # --------------------------------------------------------
+    # 3 joinpoints
+    # --------------------------------------------------------
+    if (k >= 3) {
+      for (jp1 in data$.jp_time) {
+        for (jp2 in data$.jp_time) {
+          if (jp2 <= jp1) {
+            next
+          }
+
+          for (jp3 in data$.jp_time) {
+            if (jp3 <= jp2) {
+              next
+            }
+
+            jp <- c(
+              jp1,
+              jp2,
+              jp3
+            )
+
+            if (!valid_jp(jp)) {
+              next
+            }
+
+            mod <- fit_jp(jp)
+
+            results[[length(results) + 1]] <-
+              tibble::tibble(
+                n_jp = 3,
+                jp1 = jp1,
+                jp2 = jp2,
+                jp3 = jp3,
+                jp4 = NA_real_,
+                jp5 = NA_real_,
+                BIC = stats::BIC(mod)
+              )
+          }
+        }
+      }
+    }
+
+    # --------------------------------------------------------
+    # 4 joinpoints
+    # --------------------------------------------------------
+    if (k >= 4) {
+      for (jp1 in data$.jp_time) {
+        for (jp2 in data$.jp_time) {
+          if (jp2 <= jp1) {
+            next
+          }
+
+          for (jp3 in data$.jp_time) {
+            if (jp3 <= jp2) {
+              next
+            }
+
+            for (jp4 in data$.jp_time) {
+              if (jp4 <= jp3) {
+                next
+              }
+
+              jp <- c(
+                jp1,
+                jp2,
+                jp3,
+                jp4
+              )
+
+              if (!valid_jp(jp)) {
+                next
+              }
+
+              mod <- fit_jp(jp)
+
+              results[[length(results) + 1]] <-
+                tibble::tibble(
+                  n_jp = 4,
+                  jp1 = jp1,
+                  jp2 = jp2,
+                  jp3 = jp3,
+                  jp4 = jp4,
+                  jp5 = NA_real_,
+                  BIC = stats::BIC(mod)
+                )
+            }
+          }
+        }
+      }
+    }
+
+    # --------------------------------------------------------
+    # 5 joinpoints
+    # --------------------------------------------------------
+    if (k >= 5) {
+      for (jp1 in data$.jp_time) {
+        for (jp2 in data$.jp_time) {
+          if (jp2 <= jp1) {
+            next
+          }
+
+          for (jp3 in data$.jp_time) {
+            if (jp3 <= jp2) {
+              next
+            }
+
+            for (jp4 in data$.jp_time) {
+              if (jp4 <= jp3) {
+                next
+              }
+
+              for (jp5 in data$.jp_time) {
+                if (jp5 <= jp4) {
+                  next
+                }
+
+                jp <- c(
+                  jp1,
+                  jp2,
+                  jp3,
+                  jp4,
+                  jp5
+                )
+
+                if (!valid_jp(jp)) {
+                  next
+                }
+
+                mod <- fit_jp(jp)
+
+                results[[length(results) + 1]] <-
+                  tibble::tibble(
+                    n_jp = 5,
+                    jp1 = jp1,
+                    jp2 = jp2,
+                    jp3 = jp3,
+                    jp4 = jp4,
+                    jp5 = jp5,
+                    BIC = stats::BIC(mod)
+                  )
+              }
+            }
+          }
+        }
+      }
+    }
+
+    # --------------------------------------------------------
+    # Full list of models
+    # --------------------------------------------------------
+    results <- dplyr::bind_rows(results) |>
+      dplyr::arrange(BIC)
+
+    # --------------------------------------------------------
+    # Select best model
+    # --------------------------------------------------------
+    best <- results |>
+      dplyr::slice_min(
+        BIC,
+        n = 1,
+        with_ties = FALSE
+      )
+
+    # --------------------------------------------------------
+    # Selected joinpoints
+    # --------------------------------------------------------
+    joinpoints <- best |>
+      dplyr::select(
+        jp1,
+        jp2,
+        jp3,
+        jp4,
+        jp5
+      ) |>
+      unlist(
+        use.names = FALSE
+      ) |>
+      stats::na.omit()
+
+    # --------------------------------------------------------
+    # Final model
+    # --------------------------------------------------------
+    model <- fit_jp(joinpoints)
+
+    # --------------------------------------------------------
+    # Return
+    # --------------------------------------------------------
+    list(
+      model = model,
+      joinpoints = joinpoints,
+      BIC = best$BIC,
+      results = results
     )
   }
 
-  # ---- Create control structure ----
-  seg_ctrl <- segmented::seg.control(
-    min.nj = min_dist
+  # ==========================================================
+  # Fit each group
+  # ==========================================================
+  data_split <- data |>
+    dplyr::group_by(.jp_group) |>
+    dplyr::group_split()
+
+  mods <- purrr::map(
+    data_split,
+    fit_group
   )
 
-  # ---- Fit joinpoint regression by groups ----
-  if (step) {
-    mods <- data |>
-      dplyr::group_map(
-        ~ {
-          mod <- segmented::selgmented(
-            olm = lm_fit(.x),
-            Kmax = k,
-            type = "bic",
-            th = 2,
-            stop.if = 4,
-            check.dslope = test,
-            control = seg_ctrl,
-            msg = FALSE
-          )
-
-          mod$call <- substitute(
-            segmented::selgmented(
-              olm = stats::lm(
-                formula = log(Y) ~ X
-              ),
-              Kmax = K
-            ),
-            list(
-              Y = value,
-              X = time,
-              K = k
-            )
-          )
-
-          mod
-        }
-      )
-  } else {
-    mods <- data |>
-      dplyr::group_map(
-        ~ {
-          mod <- segmented::segmented(
-            obj = lm_fit(.x),
-            seg.Z = ~.jp_time,
-            npsi = k,
-            control = seg_ctrl
-          )
-
-          mod$call <- substitute(
-            segmented::segmented(
-              obj = stats::lm(
-                formula = log(Y) ~ X
-              ),
-              seg.Z = ~X,
-              npsi = K
-            ),
-            list(
-              Y = value,
-              X = time,
-              K = k
-            )
-          )
-
-          mod
-        }
-      )
-  }
-
   # ---- Name models ----
-  mods <- rlang::set_names(mods, groups)
+  names(mods) <- as.character(groups)
 
-  # ---- Messages ----
+  # ---- Message ----
   purrr::iwalk(
     mods,
     ~ {
-      jp <- tryCatch(.x$psi[, 2], error = \(e) numeric())
+      jp <- .x$joinpoints
 
       message(
-        "Model: ",
-        .y,
+        if (is.na(.y)) {
+          "Model"
+        } else {
+          .y
+        },
         " | Joinpoint(s): ",
-        dplyr::if_else(
-          length(jp) == 0,
-          "no significant joinpoints detected",
-          paste(scales::number(jp, big.mark = ""), collapse = "; ")
-        )
+        if (length(jp) == 0) {
+          "No joinpoints detected"
+        } else {
+          paste(
+            scales::number(
+              jp,
+              big.mark = ""
+            ),
+            collapse = "; "
+          )
+        }
       )
     }
   )
+
+  # ---- Return ----
+  mods
 }
